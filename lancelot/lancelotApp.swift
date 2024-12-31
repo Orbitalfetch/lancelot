12c223fb946e534009a46375d5171cfd19125615
boredcoder411 <taamarcos@gmail.com>
2024-12-31 15:22:32 +0100

    Initial Commit

---
+++ lancelot/lancelotApp.swift
@@ -0,0 +1,17 @@
+//
+//  lancelotApp.swift
+//  lancelot
+//
+//  Created by Theo Marcos on 31.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct lancelotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
